b9d47ca585416fd3e6cbf624a51855ef31d2530a
Kinitto <joaquingomezinfor@gmail.com>
2021-12-13 14:26:23 +0100

    region y ciudad

--- src/app/pais/pages/por-capital/por-capital.component.ts
+++ src/app/pais/pages/por-capital/por-capital.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisServiceService } from '../../services/pais-service.service';
 
 @Component({
   selector: 'app-por-capital',
@@ -8,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorCapitalComponent implements OnInit {
 
-  constructor() { }
-
+  paises: Country[]=[];
+  constructor(private paisService:PaisServiceService) { }
+  termino: string='';
   ngOnInit(): void {
+  }
+  buscar( termino :string){
+    this.termino=termino;
+    this.paisService.buscarCapital(this.termino)
+    .subscribe(resp=>
+      {
+        this.paises = resp;
+        console.log(this.paises);
+      })
+
   }
 
 }
+
--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisServiceService } from '../../services/pais-service.service';
 
 @Component({
   selector: 'app-por-region',
@@ -8,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorRegionComponent implements OnInit {
 
-  constructor() { }
-
+  paises: Country[]=[];
+  constructor(private paisService:PaisServiceService) { }
+  termino: string='';
   ngOnInit(): void {
+  }
+  buscar( termino :string){
+    this.termino=termino;
+    this.paisService.buscarRegion(this.termino)
+    .subscribe(resp=>
+      {
+        this.paises = resp;
+        console.log(this.paises);
+      })
+
   }
 
 }
